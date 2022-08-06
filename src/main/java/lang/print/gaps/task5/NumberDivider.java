dd012e0166e87cec97c5ed71c2d1d0981e7f5860
Shushoook <shushoook666@gmail.com>
2022-08-06 13:28:14 +0400

    stage0-module3-task5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,10 +1,8 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    public static final int FIVE = 5;
-
-    void divide(int toBeDivided) {
-        System.out.println(toBeDivided / 5);
-    }
-
+     void divide(int toBeDivided){
+         double result  = toBeDivided/5.0;
+         System.out.println(result);
+     }
 }
